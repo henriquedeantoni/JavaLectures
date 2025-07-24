5293aa18738cf6b6784dc3d8425c613e05cbef7b
Henrique de Antoni <henriqdeantoni@gmail.com>
2025-07-23 23:05:59 -0300

    function lambda challenge 1

---
+++ StreamChallenge1/src/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter full file path: ");
+		String path = sc.nextLine();
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		sc.close();
+
+	}
+
+}
